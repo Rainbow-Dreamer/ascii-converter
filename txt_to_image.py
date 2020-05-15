0a373463292ff17a14c2b3c089bd0ea297927eca
Rainbow-Dreamer <59408627+Rainbow-Dreamer@users.noreply.github.com>
2020-05-15 19:12:05 -0400

    update

---
+++ txt_to_image.py
@@ -0,0 +1,64 @@
+import PIL
+import PIL.Image
+import PIL.ImageFont
+import PIL.ImageOps
+import PIL.ImageDraw
+
+PIXEL_ON = 0  # PIL color to use for "on"
+PIXEL_OFF = 255  # PIL color to use for "off"
+PIL.Image.MAX_IMAGE_PIXELS = None
+
+def convert(txt_name, img_name, mode=0, font_path=None):
+    image = text_image(txt_name, mode=mode, font_path=font_path)
+    image.save(img_name)
+
+
+def text_image(lines, font_path=None, mode=0):
+    """Convert text file to a grayscale image with black characters on a white background.
+
+    arguments:
+    text_path - the content of this file will be converted to an image
+    font_path - path to a font file (for example impact.ttf)
+    """
+    grayscale = 'L'
+    if mode == 1:
+        with open(lines) as text_file:  # can throw FileNotFoundError
+            lines = tuple(l.rstrip() for l in text_file.readlines())
+    else:
+        lines = tuple(lines.split('\n'))
+    # parse the file into lines
+    # choose a font (you can see more detail in my library on github)
+    large_font = 20  # get better resolution with larger size
+    font_path = font_path or 'cour.ttf'  # Courier New. works in windows. linux may need more explicit path
+    try:
+        font = PIL.ImageFont.truetype(font_path, size=large_font)
+    except IOError:
+        font = PIL.ImageFont.load_default()
+        print('Could not use chosen font. Using default.')
+
+    # make the background image based on the combination of font and lines
+    pt2px = lambda pt: int(round(pt * 96.0 / 72))  # convert points to pixels
+    max_width_line = max(lines, key=lambda s: font.getsize(s)[0])
+    # max height is adjusted down because it's too large visually for spacing
+    test_string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    max_height = pt2px(font.getsize(test_string)[1])
+    max_width = pt2px(font.getsize(max_width_line)[0])
+    height = max_height * len(lines)  # perfect or a little oversized
+    width = int(round(max_width + 40))  # a little oversized
+    image = PIL.Image.new(grayscale, (width, height), color="white")
+    draw = PIL.ImageDraw.Draw(image)
+
+    # draw each line of text
+    vertical_position = 5
+    horizontal_position = 5
+    line_spacing = int(round(max_height * 0.8))  # reduced spacing seems better
+    for line in lines:
+        draw.text((horizontal_position, vertical_position),
+                  line,
+                  fill="black",
+                  font=font)
+        vertical_position += line_spacing
+    # crop the text
+    c_box = PIL.ImageOps.invert(image).getbbox()
+    image = image.crop(c_box)
+    return image
